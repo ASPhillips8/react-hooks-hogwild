9ac2c086ab200c76af5c03d3a27f9248031dd11d
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-06-16 13:18:50 -0400

    Updated Form details

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,12 +7,14 @@ function Form({onAddPig}) {
 	return (
 		<div>
       <form onSubmit={onAddPig}>
-        <input type="text" name="name" placeholder="Name"></input>
-        <input type="text" name="name" placeholder="Name"></input>
-        <input type="text" name="name" placeholder="Name"></input>
-        <input type="text" name="name" placeholder="Name"></input>
-        <input type="text" name="name" placeholder="Name"></input>
-        <input type="text" name="name" placeholder="Name"></input>
+        <input type="text" id="name" name="name" placeholder="Name"></input>
+        <input type="text" id="image" name="image" placeholder="Image URL"></input>
+        <input type="text" id="specialty" name="specialty" placeholder="Specialty"></input>
+        <input type="text" id="weight" name="weight" placeholder="Weight"></input>
+        <input type="text" id="medal" name="medal" placeholder="Highest Medal Achieved"></input>
+        <label>Greased?</label>
+        <input type="checkbox" id="greased" name="Greased"/>
+        <button type="submit">Add Pig</button>
       </form>
 		</div>
 	);
